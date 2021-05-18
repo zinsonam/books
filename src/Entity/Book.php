9913521e110d05aeec16ff81b902dddcc755222b
zinsonam <75520059+zinsonam@users.noreply.github.com>
2021-05-18 13:27:18 +0200

    check

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -46,7 +46,7 @@ class Book
      * @ORM\Column(type="float")
      */
     private $prix;
-
+//ajouter cascade={"persist"} pour le problem de liaison enter categorie et book
     /**
      * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="books", cascade={"persist"})
      */
