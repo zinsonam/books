b1a2b287a12615d4ef6ee4c549454dc769a2f899
zinsonam <75520059+zinsonam@users.noreply.github.com>
2021-05-19 12:19:02 +0200

    api complet

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,7 +6,8 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\Serializer\Annotation\Groups;
+                                                                        
 /**
  * @ORM\Entity(repositoryClass=CategoryRepository::class)
  */
@@ -21,6 +22,7 @@ class Category
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"book_get"})
      */
     private $name;
 
